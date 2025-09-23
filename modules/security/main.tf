c6b0ba391451a6263868ca4addb3061d5461626a
WahyuHR <38802961+wahyuhr18@users.noreply.github.com>
2025-09-23 22:28:13 +0700

    Update modules/security/main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,4 +1,4 @@
-# Bastion SG (SSH dari internet)
+# Bastion SG (SSH from internet)
 resource "aws_security_group" "bastion" {
   name        = "bastion_sg"
   description = "Bastion Security Group"
@@ -9,7 +9,7 @@ resource "aws_security_group" "bastion" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.ssh_cidr] # hanya IP publik kamu
+    cidr_blocks = [var.ssh_cidr]
   }
 
   egress {
@@ -30,53 +30,20 @@ resource "aws_security_group" "k8s_master" {
   description = "Kubernetes Master Security Group"
   vpc_id      = var.vpc_id
 
+  # SSH from Bastion
   ingress {
-    description     = "SSH from Bastion"
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion.id]
   }
 
+  # Allow all traffic inside master SG (ETCD, Kubelet, etc.)
   ingress {
-    description     = "K8s API Server"
-    from_port       = 6443
-    to_port         = 6443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.k8s_worker.id]
-  }
-
-  ingress {
-    description     = "ETCD (cluster-internal only)"
-    from_port       = 2379
-    to_port         = 2380
-    protocol        = "tcp"
-    self            = true
-  }
-
-  ingress {
-    description     = "Weavenet communication"
-    from_port       = 6783
-    to_port         = 6784
-    protocol        = "tcp"
-    security_groups = [aws_security_group.k8s_worker.id]
-  }
-
-  ingress {
-    description     = "Kubelet & Control Plane Ports"
-    from_port       = 10248
-    to_port         = 10260
-    protocol        = "tcp"
-    self            = true
-    security_groups = [aws_security_group.k8s_worker.id]
-  }
-
-  ingress {
-    description     = "NodePort Services"
-    from_port       = 30000
-    to_port         = 32767
-    protocol        = "tcp"
-    security_groups = [aws_security_group.k8s_worker.id]
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
   }
 
   egress {
@@ -97,45 +64,20 @@ resource "aws_security_group" "k8s_worker" {
   description = "Kubernetes Worker Security Group"
   vpc_id      = var.vpc_id
 
+  # SSH from Bastion
   ingress {
-    description     = "SSH from Bastion"
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion.id]
   }
 
+  # Allow all traffic inside worker SG
   ingress {
-    description     = "K8s API Server Access"
-    from_port       = 6443
-    to_port         = 6443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.k8s_master.id]
-  }
-
-  ingress {
-    description     = "Weavenet communication"
-    from_port       = 6783
-    to_port         = 6784
-    protocol        = "tcp"
-    security_groups = [aws_security_group.k8s_master.id]
-  }
-
-  ingress {
-    description     = "Kubelet & Node communication"
-    from_port       = 10248
-    to_port         = 10260
-    protocol        = "tcp"
-    self            = true
-    security_groups = [aws_security_group.k8s_master.id]
-  }
-
-  ingress {
-    description     = "NodePort Services"
-    from_port       = 30000
-    to_port         = 32767
-    protocol        = "tcp"
-    security_groups = [aws_security_group.k8s_master.id]
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
   }
 
   egress {
@@ -148,4 +90,72 @@ resource "aws_security_group" "k8s_worker" {
   tags = {
     Name = "k8s_worker_sg"
   }
+}
+
+# --- Cross-SG rules ---
+
+# K8s API (Master → Worker)
+resource "aws_security_group_rule" "api_master_to_worker" {
+  type                     = "ingress"
+  from_port                = 6443
+  to_port                  = 6443
+  protocol                 = "tcp"
+  security_group_id         = aws_security_group.k8s_worker.id
+  source_security_group_id  = aws_security_group.k8s_master.id
+  description              = "K8s API access from master to worker"
+}
+
+# WeaveNet (Master ↔ Worker)
+resource "aws_security_group_rule" "weavenet_master_to_worker" {
+  type                     = "ingress"
+  from_port                = 6783
+  to_port                  = 6784
+  protocol                 = "tcp"
+  security_group_id         = aws_security_group.k8s_master.id
+  source_security_group_id  = aws_security_group.k8s_worker.id
+  description              = "WeaveNet traffic from worker to master"
+}
+
+# NodePort services (Master → Worker)
+resource "aws_security_group_rule" "nodeport_master_to_worker" {
+  type                     = "ingress"
+  from_port                = 30000
+  to_port                  = 32767
+  protocol                 = "tcp"
+  security_group_id         = aws_security_group.k8s_worker.id
+  source_security_group_id  = aws_security_group.k8s_master.id
+  description              = "NodePort services from master to worker"
+}
+
+# Kubelet / Control Plane (Master intra-cluster)
+resource "aws_security_group_rule" "kubelet_master" {
+  type                     = "ingress"
+  from_port                = 10248
+  to_port                  = 10260
+  protocol                 = "tcp"
+  security_group_id         = aws_security_group.k8s_master.id
+  source_security_group_id  = aws_security_group.k8s_master.id
+  description              = "Kubelet & Control Plane intra-master"
+}
+
+# Kubelet / Node communication (Worker intra-cluster)
+resource "aws_security_group_rule" "kubelet_worker" {
+  type                     = "ingress"
+  from_port                = 10248
+  to_port                  = 10260
+  protocol                 = "tcp"
+  security_group_id         = aws_security_group.k8s_worker.id
+  source_security_group_id  = aws_security_group.k8s_worker.id
+  description              = "Kubelet & Node intra-worker"
+}
+
+# ETCD (Master intra-cluster)
+resource "aws_security_group_rule" "etcd_master" {
+  type                     = "ingress"
+  from_port                = 2379
+  to_port                  = 2380
+  protocol                 = "tcp"
+  security_group_id         = aws_security_group.k8s_master.id
+  source_security_group_id  = aws_security_group.k8s_master.id
+  description              = "ETCD intra-master"
 }
